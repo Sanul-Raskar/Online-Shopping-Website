733f10084b823dbbe2746fe2606990ef48b025e4
Sanul-Raskar <sanulraskar@gmail.com>
2018-07-06 00:40:58 +0530

    Added both page transitions

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ setTimeout(() => {
   username.focus();
 }, 1500);
 
-function leftslidelogin() {
+function loginpage_submit() {
   document.getElementById('login_left_panel').classList.toggle('login_left_rightSlide');
   document.getElementById('login_right_panel').classList.toggle('login_right_leftSlide');
 
@@ -29,11 +29,32 @@ function leftslidelogin() {
 
   var showSignup = document.getElementById('container1');
   showSignup.style.display = "block";
-  setTimeout(function(){},1600);
+  setTimeout(function() {}, 1600);
   document.getElementById("signupcontainer").className += " signup_form_left_slide";
   document.getElementById("signup_left_panel_img").className += " login_on_signup_leftslide";
   document.getElementById("signup_left_panel_h3").className += " login_on_signup_leftslide";
   document.getElementById("signup_left_panel_button").className += " login_on_signup_leftslide";
+}
 
+function submitpage_login() {
+
+  setTimeout(function hideSubmit() {
+    var hidesubmit = document.getElementById('container1');
+    hidesubmit.style.display = "none";
+    var hidelogin = document.getElementById('container');
+    hidelogin.style.display = "block";
+  }, 100);
+
+  document.getElementById('signupcontainer').classList.remove('signup_form_left_slide');
+  document.getElementById('signup_left_panel_img').classList.remove('login_on_signup_leftslide');
+  document.getElementById('signup_left_panel_h3').classList.remove('login_on_signup_leftslide');
+  document.getElementById('signup_left_panel_button').classList.remove('login_on_signup_leftslide');
+  document.getElementById('login_right_panel').classList.remove('login_right_leftSlide');
+  document.getElementById('signupcontainer').classList.add('signup_form_right_slide');
+  document.getElementById('signup_left_panel_img').classList.add('submit_on_login_rightslide');
+  document.getElementById('signup_left_panel_h3').classList.add('submit_on_login_rightslide');
+  document.getElementById('signup_left_panel_button').classList.add('submit_on_login_rightslide');
+  document.getElementById('login_left_panel').classList.remove('login_left_rightSlide');
+  document.getElementById('login_img').classList.add('cart_on_login_leftslide');
 
 }
