a85964ced68ea118891f4fcdf50f06f837eceec7
Sanul-Raskar <sanulraskar@gmail.com>
2018-10-06 01:31:21 +0530

    Added Force Directed Graph

--- js/home.js
+++ js/home.js
@@ -1,11 +1,9 @@
-$(document).ready(function () {
-  $('#wrapper').fullpage(
-    {
-      anchors: ['firstPage', 'secondPage', 'thirdPage','lastPage'],
-      menu: '#menu',
-      scrollingSpeed: 1000
-    }
-  );
+$(document).ready(function() {
+  $('#wrapper').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+  });
 });
 
 var currentPixelCounter = 1;
@@ -13,6 +11,7 @@ var currentMacCounter = 1;
 var Section1 = document.querySelector('#section1');
 var Section2 = document.querySelector('#section2');
 var Section3 = document.querySelector('#section3');
+var Section4 = document.querySelector('#section4');
 var menuItem1 = document.querySelector('#a1');
 var menuItem2 = document.querySelector('#a2');
 var menuItem3 = document.querySelector('#a3');
@@ -61,7 +60,7 @@ function animate() {
   macbookimg.classList.toggle('animateimage');
 }
 
-setInterval(function () {
+setInterval(function() {
   if (Section2.classList.contains('active')) {
     SideBar.style.background = "linear-gradient(-80deg, #a173c0 8%, #640ab8)";
     menuItem2.classList.add('menuActive');
@@ -71,7 +70,7 @@ setInterval(function () {
   }
 }, 500);
 
-setInterval(function () {
+setInterval(function() {
   if (Section1.classList.contains('active')) {
     SideBar.style.background = "linear-gradient(-80deg, #ff8080 8%, #ff1a1a)";
     menuItem1.classList.add('menuActive');
@@ -81,13 +80,23 @@ setInterval(function () {
   }
 }, 500);
 
-setInterval(function () {
+setInterval(function() {
   if (Section3.classList.contains('active')) {
     SideBar.style.background = "linear-gradient(-80deg,#e27ba6 8%,  #aa0b40)";
     menuItem3.classList.add('menuActive');
     menuItem2.classList.remove('menuActive');
     menuItem1.classList.remove('menuActive');
     menuItem4.classList.remove('menuActive');
+  }
+}, 500);
+
+setInterval(function() {
+  if (Section4.classList.contains('active')) {
+    SideBar.style.background = "linear-gradient(-80deg,#ffd9b3 8%,  #804000)";
+    menuItem4.classList.add('menuActive');
+    menuItem2.classList.remove('menuActive');
+    menuItem1.classList.remove('menuActive');
+    menuItem3.classList.remove('menuActive');
   }
 }, 500);
 
@@ -317,11 +326,181 @@ function macbookBackArrow() {
 }
 
 
+/*Graph*/
+var w = 1000,
+  h = 700,
+  circleWidth = 5;
+
+var palette = {
+  "lightgray": "#E5E8E8",
+  "gray": "#708284",
+  "mediumgray": "#536870",
+  "blue": "#3B757F",
+  "red": "#ff0000",
+  "black": "#000000",
+  "brown": "#5d2626",
+  "violet": "#aa0b95"
+
+}
+
+var colors = d3.scale.category20();
+
+var nodes = [{
+    name: ""
+  },
+  {
+    name: "Meet The Team",
+    target: [0, 1, 2],
+    value: 85
+  },
+  {
+    name: "Sanul",
+    target: [0],
+    value: 90
+  },
+  {
+    name: "Manas",
+    target: [0, 1, 2],
+    value: 80
+  },
+  {
+    name: "Sachin",
+    target: [0, 1, 2],
+    value: 80
+  },
+];
+
+// Array for linked nodes
+var links = [];
+
+// Link nodes together and push them in the links array
+for (var i = 0; i < nodes.length; i++) {
+  if (nodes[i].target !== undefined) {
+    for (var x = 0; x < nodes[i].target.length; x++)
+      links.push({
+        source: nodes[i],
+        target: nodes[nodes[i].target[x]]
+      });
+  };
+};
+
+var myChart = d3.select('.graph')
+  .append("div")
+  .classed("svg-container", true)
+
+  .append('svg')
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 1000 800")
+  .classed("svg-content-responsive", true);
+
+// Settings for the force repulsion
+var force = d3.layout.force()
+  .nodes(nodes)
+  .links([])
+  .gravity(0.1)
+  .charge(-2500)
+  .size([w, h]);
+
+// Draw links first
+var link = myChart.selectAll('line')
+  .data(links).enter().append('line')
+  .attr('stroke', palette.lightgray)
+  .attr('strokewidth', '1');
+
+// Draw nodes on top of links
+var node = myChart.selectAll('circle')
+  .data(nodes).enter()
+  .append('g')
+  .call(force.drag);
 
 
+node.append('circle')
+  .attr('cx', function(d) {
+    return d.x;
+  })
+  .attr('cy', function(d) {
+    return d.y;
+  })
+  .attr('r', function(d, i) {
+    console.log(d.value);
+    if (i > 0) {
+      return circleWidth + d.value;
+    } else {
+      return circleWidth + 35;
+    }
+  })
+  .attr('fill', function(d, i) {
+    if (i > 0) {
+      return colors(i);
+    } else {
+      return '#fff';
+    }
+  })
+  .attr('strokewidth', function(d, i) {
+    if (i > 0) {
+      return '0';
+    } else {
+      return '2';
+    }
+  })
+  .attr('stroke', function(d, i) {
+    if (i > 0) {
+      return '';
+    } else {
+      return 'black';
+    }
+  });
 
+// User interaction when we click and move a node
+force.on('tick', function(e) {
+  node.attr('transform', function(d, i) {
+    return 'translate(' + d.x + ',' + d.y + ')'
+  });
 
+  link
+    .attr('x1', function(d) {
+      return d.source.x;
+    })
+    .attr('y1', function(d) {
+      return d.source.y;
+    })
+    .attr('x2', function(d) {
+      return d.target.x;
+    })
+    .attr('y2', function(d) {
+      return d.target.y;
+    })
+});
 
+// Add text to the nodes
+node.append('text')
+  .text(function(d) {
+    return d.name;
+  })
+  .attr('font-family', 'Raleway', 'Helvetica Neue, Helvetica')
+  .attr('fill', function(d, i) {
+    console.log(d.value);
+    if (i > 0 && d.value < 10) {
+      return palette.black;
+    } else if (i > 0 && d.value > 10) {
+      return palette.black;
+    } else {
+      return palette.blue;
+    }
+  })
+  .attr('text-anchor', function(d, i) {
+    return 'middle';
+  })
+  .attr('font-size', function(d, i) {
+    if (i > 0) {
+      return '1.2em';
+    } else {
+      return '1.2em';
+    }
+  });
+
+// Display the graph and start reacting to events
+force.start();
 
 
 
